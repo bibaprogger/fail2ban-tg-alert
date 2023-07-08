0a3915aa606aeb0576dd946e4acbf6fb89e12794
Падий Николай Николаевич <padiynn@mosreg.ru>
2023-07-08 23:03:38 +0300

    job done

--- main.py
+++ main.py
@@ -2,38 +2,46 @@
 from f2b_parser import parse
 import requests
 import time
+import os
+
+
+LOGNAME = '/var/log/fail2ban.log'
 
 
 def send_telegram_message(action: str, ip: str, dt: str) -> None:
-    TOKEN = '5878794044:AAGQBZyT1NyN6JoFrt675EcHTMT-zrQlAZ4'
-    CHAT_ID = '-879794776'
+    TOKEN = 'your token'
+    CHAT_ID = 'your chat id'
     message = f'[ALERT] {dt} {action} {ip}'
     url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}'
-    requests.get(url)
+    requests.post(url)
 
 
-def mainloop(logfile: str) -> None:
-    try:
-        with open(logfile, 'r') as f:
-            f.seek(0, 2)
-            while True:
-                line = ''
-                while len(line) == 0 or line[-1] != '\n':
-                    tail = f.readline()
-                    if tail == '':
-                        time.sleep(0.1)
-                        continue
-                    line += tail
-                parsed_data = parse(line)
-                if parsed_data is not None:
-                    send_telegram_message(parsed_data['action'], parsed_data['ip'], parsed_data['dt'])
-    except KeyboardInterrupt as msg:
-        print('Ctrl+C Pressed. Shutdown')
-        exit(0)
+def tail(file) -> None:
+    file.seek(0,2)
+    inode = os.fstat(file.fileno()).st_ino
+
+    while True:
+        line = file.readline()
+        if not line:
+            time.sleep(0.1)
+            if os.stat(LOGNAME).st_ino != inode:
+                file.close()
+                file = open(LOGNAME, 'r')
+                inode = os.fstat(file.fileno()).st_ino
+            continue
+        yield line
+
+
+def mainloop(logfile) -> None:
+    for line in tail(logfile):
+        parsed_data = parse(line)
+        if parsed_data is not None:
+                send_telegram_message(parsed_data['action'], parsed_data['ip'], parsed_data['dt'])
+
 
 
 def main() -> None:
-    logfile = '/var/log/fail2ban.log'
+    logfile = open(LOGNAME, 'r')
     mainloop(logfile)
 
 
